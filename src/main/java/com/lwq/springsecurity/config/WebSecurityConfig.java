5840b64a2ce9edb9999f0b414bce263464bbebad
lwq <2065277594@qq.com>
2022-12-07 22:29:38 +0800

    OncePerJWTFilter

--- src/main/java/com/lwq/springsecurity/config/WebSecurityConfig.java
+++ src/main/java/com/lwq/springsecurity/config/WebSecurityConfig.java
@@ -1,16 +1,11 @@
 package com.lwq.springsecurity.config;
 
-import com.lwq.springsecurity.filter.JWTFilter;
+import com.lwq.springsecurity.filter.OncePerJWTFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,7 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecurityConfig {
 
     @Autowired
-    private JWTFilter jwtFilter;
+    private OncePerJWTFilter jwtFilter;
 
     @Autowired
     private AuthenticationEntryPoint authenticationEntryPoint;
--- src/main/java/com/lwq/springsecurity/filter/OncePerJWTFilter.java
+++ src/main/java/com/lwq/springsecurity/filter/OncePerJWTFilter.java
@@ -21,58 +21,58 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * 所有请求校验的过滤器,?
- *      写完之后还要配置一下在整个security过滤器链里的顺序
+ * 所有请求校验的过滤器
+ * 写完之后还要配置一下在整个security过滤器链里的顺序
  */
 
 @Component
-public class JWTFilter extends OncePerRequestFilter {
+public class OncePerJWTFilter extends OncePerRequestFilter {
 
     @Autowired
-    private  RedisUtils redisUtils;
+    private RedisUtils redisUtils;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //获取token
         String token = request.getHeader("token");
-        if( token== null || "".equals(token)){
-            //放行,
-            filterChain.doFilter(request,response);
-            return;  //不return的话后面的过滤器执行完还会在继续执行后面的
+        if (token == null || "".equals(token)) {
+            //没有token说明是第一次登录,放行,走后面的security过滤器
+            filterChain.doFilter(request, response);
+            return;  //不return的话后面的过滤器执行完还会在继续执行下面的代码
         }
-        //解析token
+        //有token开始解析token,获取其中保存的id
         Long userId = JwtHelper.getUserId(token);
-        if(userId==null){
-            //使用security提供的认证/授权错误处理(不好用,还会跟全局异常冲突)
+        if (userId == null) {
+            //直接抛出,配置security提供的认证/授权错误处理(不好用,还会跟全局异常冲突)
             //throw new RuntimeException("token令牌有错误");
 
             RuntimeException e = new RuntimeException("token令牌有错误");
-            //处理filter的异常
+            //处理filter的异常(将异常发送到一个controller里去处理)
             request.setAttribute("exception", e);
             request.getRequestDispatcher("/filterExceptionHandler").forward(request, response);
         }
-        //redis中获取用户信息
-        String json = redisUtils.get("login:"+String.valueOf(userId));
-        if( json== null || "".equals(json)){
-            //使用security提供的认证/授权错误处理(不好用,还会跟全局异常冲突)
+        //根据token中的id去redis中获取用户信息
+        String json = redisUtils.get("login:" + String.valueOf(userId));
+        if (json == null || "".equals(json)) {
+            //直接抛出,配置security提供的认证/授权错误处理(不好用,还会跟全局异常冲突)
             //throw new RuntimeException("用户登录已过期,请重新登陆");
 
             RuntimeException e = new RuntimeException("用户登录已过期,请重新登陆");
-            //处理filter的异常
+            //处理filter的异常(将异常发送到一个controller里去处理)
             request.setAttribute("exception", e);
             request.getRequestDispatcher("/filterExceptionHandler").forward(request, response);
         }
+        //将redis中的数据反序列化成对象
         LoginUser loginUser = JSON.parseObject(json, LoginUser.class);
 
-
-
-        //封装authenticationToken(使用三个参数这个方法,第三个参数是权限信息,内部会设置这个对象是已经认证过的)
+        //封装成UsernamePasswordAuthenticationToken
+        // (使用三个参数这个方法,第三个参数是权限信息,这个方法内部会设置这个对象是已经认证成功过的)
         UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(loginUser,null,loginUser.getAuthorities());
+                new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
         //存入SecurityContextHolder
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
         //放行
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(request, response);
     }
 }

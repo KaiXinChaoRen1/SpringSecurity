0365d73b39eb8bc80187dbabaee7f2b59f8856a1
lwq <2065277594@qq.com>
2022-12-07 22:38:54 +0800

    处理过滤器中发生的异常

--- src/main/java/com/lwq/springsecurity/controller/FilterExceptionController.java
+++ src/main/java/com/lwq/springsecurity/controller/FilterExceptionController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 本项目中专门处理过滤器中发生的异常,获取异常并抛出由全局异常处理器处理,返回前端统一的数据
+ */
+
 @RestController
 public class FilterExceptionController {
 
